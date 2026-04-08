a6b03c15132e64c978253bc8e5455df2c2f18ac5
Alexandr Esetov <aalexesetov2541@gmail.com>
2026-04-08 19:11:51 +0200

    initial commit

---
+++ src/main/java/personal/jarvis_plan_my_day/JarvisPlanMyDayApplication.java
@@ -0,0 +1,13 @@
+package personal.jarvis_plan_my_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisPlanMyDayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarvisPlanMyDayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/jarvis_plan_my_day/JarvisPlanMyDayApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.jarvis_plan_my_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisPlanMyDayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
